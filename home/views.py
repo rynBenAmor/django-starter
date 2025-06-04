1e5f34bbf41608a6707318893579a96b829af59e
Rayen <153410746+rynBenAmor@users.noreply.github.com>
2025-06-04 16:41:04 +0100

    26th commit : added a 2nd toast component

--- home/views.py
+++ home/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 
 def home_view(request):
+
+    messages.success(request, 'success , lorem ipsm soemthi sdas auto der nacht')
+    messages.error(request, 'error , lorem ipsm soemthi sdas auto der nacht')
+    messages.warning(request, 'warning , lorem ipsm soemthi sdas auto der nacht')
+    messages.info(request, 'info , lorem ipsm soemthi sdas auto der nacht')
+
     return render(request, "home/home.html")
 
 
